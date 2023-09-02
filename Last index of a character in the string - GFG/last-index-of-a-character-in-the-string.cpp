3205a75d97503a6303e0b275d08c9890c97c061e
Ajay Shakya <124959219+ajayhithub@users.noreply.github.com>
2023-09-02 14:28:25 +0530

    Added solution - LeetHub

---
+++ Last index of a character in the string - GFG/last-index-of-a-character-in-the-string.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    int LastIndex(string s, char p){
+        //complete the function here
+        int n = s.length();
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]==p)
+            return i;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        char p;
+        cin >> p;
+        Solution ob;
+        cout << ob.LastIndex(s, p) << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
