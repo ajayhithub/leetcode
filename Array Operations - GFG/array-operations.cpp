d810d9771fd5b5b1dc18625f3d782bd8da861adf
Ajay Shakya <124959219+ajayhithub@users.noreply.github.com>
2023-09-23 12:58:09 +0530

    Added solution - LeetHub

--- Array Operations - GFG/array-operations.cpp
+++ Array Operations - GFG/array-operations.cpp
@@ -8,38 +8,36 @@ class Solution {
   public:
     int arrayOperations(int n, vector<int> &arr) {
         // code here
-
-        
-        
         int ans = 0;
+        int t = 0;
+        
         for(int i=0;i<n;i++)
         {
             if(arr[i]==0)
-            {
-                ans++;
-            }
+            ans++;
         }
-        int count=0;
         
-        for(int i=0;i<n-1;i++)
+        if(ans == n)
+        return 0;
+        else if(ans == 0)
+        return -1;
+        
+        
+        ans = 0;
+        
+        for(int i=0;i<n;i++)
         {
-            if(arr[i]!=0 && arr[i+1]==0)
+            if(arr[i]!=0 && arr[i+1]==0 && i<n-1)
             {
-                count++;
+              ans++;
             }
+          
         }
+   
         if(arr[n-1]!=0)
-        {
-            count++;
-        }
-        if(ans==0)
-        {
-            return -1;
-        }
-        else
-        {
-            return count;
-        }
+        return ans+1;
+        else 
+        return ans;
     }
 };
 
