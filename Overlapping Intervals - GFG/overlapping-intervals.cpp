86af142011a5cde09af26f2332d35c2037cfd9cb
Ajay Shakya <124959219+ajayhithub@users.noreply.github.com>
2023-10-01 22:26:45 +0530

    Added solution - LeetHub

---
+++ Overlapping Intervals - GFG/overlapping-intervals.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
+         // Code here
+         vector<vector<int>> ans;
+         
+         sort(intervals.begin(),intervals.end());
+         int n = intervals.size();
+         
+         
+         int i = 0; 
+        while(i<n)
+        {
+           vector<int> a;
+           int st = intervals[i][0];
+           int end = intervals[i][1];
+            
+            int j = i+1; 
+           while(j<n && st<=intervals[j][0] && intervals[j][0]<=end)
+           {
+               
+              end = max(end,intervals[j][1]); 
+              j++;
+           }
+           
+           i = j;
+           a.push_back(st);
+           a.push_back(end);
+         
+         
+           ans.push_back(a);
+        }
+         
+         return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>Intervals(n);
+		for(int i = 0; i < n; i++){
+			int x, y;
+			cin >> x >> y;
+			Intervals[i].push_back(x);
+			Intervals[i].push_back(y);
+		}
+		Solution obj;
+		vector<vector<int>> ans = obj.overlappedInterval(Intervals);
+		for(auto i: ans){
+			for(auto j: i){
+				cout << j << " ";
+			}
+		}
+		cout << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
