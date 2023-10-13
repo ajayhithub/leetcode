5d01ebfdefa229af4f7efa81bb2bcfa55133bf76
Ajay Shakya <124959219+ajayhithub@users.noreply.github.com>
2023-10-13 15:00:25 +0530

    Added solution - LeetHub

---
+++ Check whether BST contains Dead End - GFG/check-whether-bst-contains-dead-end.cpp
@@ -0,0 +1,129 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+        
+     cout<<isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+
+bool fun(Node *root, int l, int r)
+{
+    if(root==NULL)
+    return false;
+    if(root->left==NULL && root->right==NULL)
+    {
+        if(abs(l-r)==0)
+        return true;
+        else
+        return false;
+    }
+    
+    bool ll = fun(root->left,l,root->data-1);
+    bool rr = fun(root->right,root->data+1,r);
+    
+    return (ll||rr);
+}
+
+bool isDeadEnd(Node *root)
+{
+    //Your code here
+   return fun(root,1,100000);
+    
+}
