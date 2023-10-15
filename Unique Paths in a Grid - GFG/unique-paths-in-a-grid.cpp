7e9766d88e3febf9ff475b4231f3b8c86f5b63fe
Ajay Shakya <124959219+ajayhithub@users.noreply.github.com>
2023-10-15 16:58:44 +0530

    Added solution - LeetHub

--- Unique Paths in a Grid - GFG/unique-paths-in-a-grid.cpp
+++ Unique Paths in a Grid - GFG/unique-paths-in-a-grid.cpp
@@ -9,23 +9,23 @@ using namespace std;
 class Solution {
   public:
   
-  int fun(int i, int j, vector<vector<int>> &grid,vector<vector<int>> &dp, int mod)
-  {
-      if(i==0 && j==0)
-      return 1;
-      if(i<0 || j<0)
-      return 0;
-      if(grid[i][j]==0)
-      return 0;
-      if(dp[i][j]!=-1)
-      return dp[i][j];
+//   int fun(int i, int j, vector<vector<int>> &grid,vector<vector<int>> &dp, int mod)
+//   {
+//       if(i==0 && j==0)
+//       return 1;
+//       if(i<0 || j<0)
+//       return 0;
+//       if(grid[i][j]==0)
+//       return 0;
+//       if(dp[i][j]!=-1)
+//       return dp[i][j];
       
-      int l = fun(i-1,j,grid,dp,mod);
-      int r = fun(i,j-1,grid,dp,mod);
+//       int l = fun(i-1,j,grid,dp,mod);
+//       int r = fun(i,j-1,grid,dp,mod);
       
-      dp[i][j] = (l+r)%mod;
-      return (l+r)%mod;
-  }
+//       dp[i][j] = (l+r)%mod;
+//       return (l+r)%mod;
+//   }
   
     int uniquePaths(int n, int m, vector<vector<int>> &grid) {
         // code here
@@ -33,9 +33,36 @@ class Solution {
          if(grid[n-1][m-1]==0 || grid[0][0]==0)
          return 0;
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+       // vector<vector<int>> dp(n,vector<int>(m,-1));
         int mod = pow(10,9)+7;
-        return fun(n-1,m-1,grid,dp,mod);
+        // return fun(n-1,m-1,grid,dp,mod);
+        
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        dp[0][0] = 1;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 && j==0)
+                continue;
+                
+                int l=0,r=0;
+                if(i>=1)
+                l = dp[i-1][j];
+                if(j>=1)
+                r = dp[i][j-1];
+                
+                if(grid[i][j]==0)
+                dp[i][j] = 0;
+                else
+                dp[i][j] = (l+r)%mod;
+                
+            }
+        }
+        
+    
+        return dp[n-1][m-1];
     }
 };
 
