5e6f3bd5e71fe5ebbc49e2ffb2abc1f819653464
Ajay Shakya <124959219+ajayhithub@users.noreply.github.com>
2023-10-19 14:56:22 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -29,31 +29,29 @@ class Solution {
 	    return (l+r)%mod;   
 	}
   
-  int fun1(int n, int d, vector<int>& arr, int mod)
-  {
-        int tsum = 0;
-        for(int i=0;i<n;i++)
-        tsum += arr[i];
-      
-      vector<vector<int>> dp(n+1,vector<int>(tsum+1,0));
-      dp[n][d] = 1;
-      
-      
-      for(int i=n-1;i>=0;i--)
-      {
-          for(int t=0;t<=tsum;t++)
-          {
-              int r = 0;
-              if(arr[i]<=t)
-              r = dp[i+1][t-arr[i]];
-              
-              int l = dp[i+1][t];
-              dp[i][t] = (l+r)%mod;
-          }
-      }
-      
-      return dp[0][tsum];
-  }
+	int fun1(vector<int>&arr, int n, int sum)
+	{
+	     int mod = pow(10,9)+7;
+         vector<vector<int>> dp(n+1,vector<int>(sum+1,0));
+         dp[n][0]=1;
+         
+         for(int i=n-1;i>=0;i--)
+         {
+             for(int t=0;t<=sum;t++)
+             {   
+                 int tak = 0;
+                 if(arr[i] <= t)
+                 {
+                   tak = dp[i+1][t-arr[i]];          
+                 }
+                 int non  = dp[i+1][t];
+                
+                 dp[i][t] = (non+tak)%mod;
+             }
+         }
+         
+         return dp[0][sum];
+	}
   
     int countPartitions(int n, int d, vector<int>& arr) {
         // Code here
@@ -67,7 +65,7 @@ class Solution {
         int target=(tsum-d)/2;
         if(target<0)return 0;
         vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        return fun(arr,n,0,target,mod,dp);
+        return fun1(arr,n,target);
         
         // return fun1(n,d,arr,mod);
         
