8dbdb5efc2c3eda2fe292cac41eb8a5ebfc4e8f2
Ajay Shakya <124959219+ajayhithub@users.noreply.github.com>
2023-10-22 00:05:34 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -27,12 +27,40 @@ class Solution {
        return non+tak;
   }
   
+  long long int fun1(int coins[], int i, int sum)
+  {
+       int N = i;
+      vector<vector<long long int>> dp(N+1,vector<long long int>(sum+1,0));
+     
+      for(int t=0;t<=sum;t++)
+      {
+          if(t%coins[0]==0)
+          dp[0][t] = 1;
+      }
+      
+      for(int i=1;i<N;i++)
+      {
+          for(int t=0;t<=sum;t++)
+          {
+               long long int non = dp[i-1][t];
+               long long int tak = 0;
+               if(coins[i]<=t)
+               tak = dp[i][t-coins[i]];
+       
+               dp[i][t] = non+tak;
+          }
+      }
+      
+      return dp[N-1][sum];
+  }
+  
     long long int count(int coins[], int N, int sum) {
 
         // code here
-        vector<vector<long long int>> dp(N+1,vector<long long int>(sum+1,-1));
-        return fun(coins,N-1,sum, dp);
+        // vector<vector<long long int>> dp(N+1,vector<long long int>(sum+1,-1));
+        // return fun(coins,N-1,sum, dp);
         
+        return fun1(coins,N,sum);
     }
 };
 
